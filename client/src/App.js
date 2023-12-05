efad9e0db1010fd3c35dde9bc4db0fbc6eafddaf
Matej Muller <matejmuller@Matejs-MacBook-Air.local>
2023-12-05 18:29:47 +0100

    Dovršavanje aplikacije

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,42 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles'; // autofilalo mi je bez styles
+import { themeSettings } from './theme'; // autofilalo bez ./
 
 function App() {
-    return <div className="app"></div>;
+    const mode = useSelector((state) => state.mode); // dohvaca initial state iz state/index.js
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    const isAuth = Boolean(useSelector((state) => state.token)); // ako token postoji onda je autoriziran korisnikj
+
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    {/* css reset za mui */}
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<LoginPage />}
+                        />
+                        <Route
+                            path="/home"
+                            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path="/profile/:userId"
+                            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+                        />
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
